8822005c676657312255daa57d24c0a111d5b7c7
Mehran Rasheed <engr.mehranrasheed@gmail.com>
2023-10-27 12:53:18 +0200

    Code refactor

--- src/test/java/com/mrgoraya/anagram/InputValidatorTest.java
+++ src/test/java/com/mrgoraya/anagram/InputValidatorTest.java
@@ -23,7 +23,7 @@ public class InputValidatorTest {
 
     @Test
     public void testValidInputWithNumbers() {
-        String validInput = "abcdef";
+        String validInput = "abcdef123";
         // No exception should be thrown.
         inputValidator.validateInput(validInput);
     }
@@ -45,29 +45,3 @@ public class InputValidatorTest {
         assertThrows(IllegalArgumentException.class, () -> inputValidator.validateInput(emptyInput));
     }
 }
-
-
-/*
-package test.java.com.mrgoraya.anagram;
-
-import main.java.com.mrgoraya.anagram.InputValidator;
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-public class InputValidatorTest {
-    private InputValidator inputValidator;
-
-    @Before
-    public void setUp() {
-        inputValidator = new InputValidator();
-    }
-    @Test
-    public void testCleanAndSortString() {
-        assertEquals("eilnst", inputValidator.cleanAndSortInputString("Silent"));
-        assertEquals("aeinooprst", inputValidator.cleanAndSortInputString("Operations!!!"));
-    }
-
-}
-*/
